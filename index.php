98da8853dadbc291d59bf3f3ffeb859ba6b90c24
Junior Moraes <juniormoraes@MacBook-Pro-de-Junior.local>
2020-05-26 17:22:11 -0300

    First commit

---
+++ index.php
@@ -0,0 +1,45 @@
+  
+<?php
+
+$mysql = new mysqli('localhost', 'root','','blog');
+$mysql ->set_charset('utf8');
+
+if($mysql == TRUE ){
+    echo "Banco conectado";
+}else {
+    echo "Erro ao conectar";
+}
+
+
+include 'Artigo.php';
+$artigo = new Artigo();
+$artigos = $artigo->exibirTodos();
+
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <title>Meu Blog</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+    <div id="container">
+        <h1>Meu Blog</h1>
+        <?php foreach ($artigos as $artigo) : ?>
+        <h2>
+            <a href="artigo.php?id=<?php echo $artigo['id']; ?>">
+                <?php echo $artigo['titulo']; ?>
+            </a>
+        </h2>
+        <p>
+            <?php echo $artigo['conteudo']; ?>
+        </p>
+        <?php endforeach; ?>
+    </div>
+</body>
+
+</html>
+
